c6ebeec2016155cf7d9064f16e4bbb5ded0569cb
Luiz pogi <johnluizvbangsoy14@gmail.com>
2026-08-06 23:56:12 +0800

    refactor(projects): standardize route middleware order

--- src/modules/projects/project.route.js
+++ src/modules/projects/project.route.js
@@ -1,19 +1,39 @@
-import { Router } from 'express';
-import projectController from './project.controller.js';
-import { createProjectSchema, updateProjectSchema } from './project.validation.js';
-import { validate, updateValidate } from '../../middleware/auth.validation.js';
-import authenticateMiddleware from '../../middleware/authenticate.middleware.js';
-import authorizeWorkspaceRole from '../../middleware/authorization.middleware.js';
+import { Router } from "express";
+import projectController from "./project.controller.js";
+import {
+  createProjectSchema,
+  updateProjectSchema,
+} from "./project.validation.js";
+import { validate, updateValidate } from "../../middleware/auth.validation.js";
+import authenticateMiddleware from "../../middleware/authenticate.middleware.js";
+import authorizeWorkspaceRole from "../../middleware/authorization.middleware.js";
 
 const router = Router();
 
 // untested
 router.use(authenticateMiddleware.authenticate);
 
-router.post('/workspace/:workspaceId/projects', updateValidate(createProjectSchema), authorizeWorkspaceRole("OWNER", "ADMIN"), projectController.createProject);
-router.get('/workspace/:workspaceId/projects', projectController.getProjects);
-router.get('/workspace/:workspaceId/projects/:projectId', projectController.getProjectById);
-router.patch('/workspace/:workspaceId/projects/:projectId', updateValidate(updateProjectSchema), authorizeWorkspaceRole("OWNER", "ADMIN"), projectController.updateProject);
-router.delete('/workspace/:workspaceId/projects/:projectId', authorizeWorkspaceRole("OWNER", "ADMIN"), projectController.deleteProject);
+router.post(
+  "/workspaces/:workspaceId/projects",
+  authorizeWorkspaceRole("OWNER", "ADMIN"),
+  updateValidate(createProjectSchema),
+  projectController.createProject,
+);
+router.get("/workspaces/:workspaceId/projects", projectController.getProjects);
+router.get(
+  "/workspaces/:workspaceId/projects/:projectId",
+  projectController.getProjectById,
+);
+router.patch(
+  "/workspaces/:workspaceId/projects/:projectId",
+  authorizeWorkspaceRole("OWNER", "ADMIN"),
+  updateValidate(updateProjectSchema),
+  projectController.updateProject,
+);
+router.delete(
+  "/workspaces/:workspaceId/projects/:projectId",
+  authorizeWorkspaceRole("OWNER", "ADMIN"),
+  projectController.deleteProject,
+);
 
 export default router;
